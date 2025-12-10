d5fd718a875740685a15caa67974facf47fe8d50
Sravani9014 <sravanimamillapalli532@gmail.com>
2025-12-10 16:52:44 +0530

    Update axiosconfig.js

--- src/axiosconfig.js
+++ src/axiosconfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "https://ruchi-zone-backend.vercel.app",
+ baseURL: "https://ruchi-zone-backend.vercel.app/",
  // your backend URL
 });
 
